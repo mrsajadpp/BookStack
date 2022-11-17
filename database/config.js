88f23fd781accdd3043fb75a8a55867e3dd3458c
mrsajadpp <saju4753@gmail.com>
2022-11-17 15:46:11 +0530

    Basket v0.2

---
+++ database/config.js
@@ -0,0 +1,20 @@
+let MongoClient = require('mongodb').MongoClient;
+let state = {
+  db: null
+}
+function connect(done) {
+  let url = 'mongodb://localhost:27017/'
+  let dbname = 'basket';
+  MongoClient.connect(url, (err, data) => {
+    if (err) return done(err);
+    state.db = data.db(dbname);
+  });
+  done();
+}
+function get() {
+  return state.db;
+}
+module.exports = {
+  connect,
+  get
+};
